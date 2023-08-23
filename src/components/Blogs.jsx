f72455e2394459f42f87be5c1bc98d4ca7239f9c
Junaid Nayeem <junaid.n@opensenselabs.com>
2023-08-23 18:36:40 +0530

    normal fields adding

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, Upload, message } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import axios from "axios";
 
 const { TextArea } = Input;
@@ -7,18 +8,32 @@ const { TextArea } = Input;
 function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [imageFile, setImageFile] = useState(null); // New state for image file
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post("/api/blogs", {
-        title,
-        content,
+      // Create a FormData object to send image and other data
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("content", content);
+      formData.append("image", imageFile);
+
+      const response = await axios.post("/api/blogs", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Important for file uploads
+        },
       });
 
       console.log("Blog saved:", response.data);
+      message.success("Blog saved successfully");
     } catch (error) {
       console.error("Error saving blog:", error);
+      message.error("Error saving blog");
     }
+  };
+
+  const handleImageUpload = (file) => {
+    setImageFile(file);
   };
 
   return (
@@ -34,6 +49,11 @@ function App() {
             value={content}
             onChange={(e) => setContent(e.target.value)}
           />
+        </Form.Item>
+        <Form.Item label="Cover Photo">
+          <Upload beforeUpload={handleImageUpload} showUploadList={false}>
+            <Button icon={<UploadOutlined />}>Upload Cover Photo</Button>
+          </Upload>
         </Form.Item>
         <Form.Item>
           <Button type="primary" onClick={handleSubmit}>
