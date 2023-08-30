901c550d265abde35474f3ac740713f29369eca2
Abhijeet Bhandari <abhijeetbhandari84@gmail.com>
2023-08-30 19:17:15 +0530

    fixed socket.io cors issue

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,20 +1,19 @@
 import axios from "axios";
 import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
-import { List, Input, Button, Form } from 'antd';
-import { SendOutlined } from '@ant-design/icons';
+import { List, Input, Button, Form } from "antd";
+import { SendOutlined } from "@ant-design/icons";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { message } from 'antd';
+import { message } from "antd";
 
-import male_avatar from '../assets/male_avatar.jpg'
-
+import male_avatar from "../assets/male_avatar.jpg";
 
 import { useSelector } from "react-redux";
-import '../scss/Chat.scss'
-import { useNavigate } from "react-router-dom";
+import "../scss/Chat.scss";
+// import { useNavigate } from "react-router-dom";
 const baseUrl = import.meta.env.VITE_BASE_URL;
-const socket = io.connect(baseUrl);
+const socket = io(baseUrl, { withCredentials: true });
 
 // Replace with your server URL
 
@@ -33,11 +32,10 @@ const Chat = () => {
   const [chatStarted, setChatStarted] = useState(false);
   const [chatEnded, setChatEnded] = useState(false);
   const [chatEndedLocally, setChatEndedLocally] = useState(false);
-  const [chatEndedMessageDisplayed, setChatEndedMessageDisplayed] = useState(false);
+  const [chatEndedMessageDisplayed, setChatEndedMessageDisplayed] =
+    useState(false);
 
-  
-
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const chatContainerRef = useRef(null);
 
@@ -53,15 +51,14 @@ const Chat = () => {
         setTimer((prevTimer) => prevTimer - 1);
       }, 1000); // Decrease timer every second
       setTimerInterval(intervalId);
-      return () => 
-      {
-          clearInterval(intervalId); 
-          if (chatEndedLocally && !chatEndedMessageDisplayed) {
-            message.info("Chat has ended.");
-            setChatEndedMessageDisplayed(true);
-          }
-      }
-    } else if ((timer === 0 && !chatEnded) && timerInterval) {
+      return () => {
+        clearInterval(intervalId);
+        if (chatEndedLocally && !chatEndedMessageDisplayed) {
+          message.info("Chat has ended.");
+          setChatEndedMessageDisplayed(true);
+        }
+      };
+    } else if (timer === 0 && !chatEnded && timerInterval) {
       // Call the decline API here
       axios.post(
         `${baseUrl}/chat/decline-chat`,
@@ -71,12 +68,16 @@ const Chat = () => {
         },
         { headers }
       );
-        clearInterval(timerInterval);
+      clearInterval(timerInterval);
       setChatEnded(true); // Chat has ended
     }
-  }, [chatStarted, chatEnded, timer, chatEndedLocally, chatEndedMessageDisplayed]);
-  
-  
+  }, [
+    chatStarted,
+    chatEnded,
+    timer,
+    chatEndedLocally,
+    chatEndedMessageDisplayed,
+  ]);
 
   useEffect(() => {
     (async () => {
@@ -102,7 +103,7 @@ const Chat = () => {
     console.log("received message", message);
     const updatedMessage = { ...message, sent: false };
     setMessages((prevMessages) => [...prevMessages, updatedMessage]);
-   
+
     if (chatContainerRef.current) {
       const newMessageHeight = chatContainerRef.current.lastChild.clientHeight;
       chatContainerRef.current.scrollTop += newMessageHeight;
@@ -119,19 +120,21 @@ const Chat = () => {
     }
 
     if (message.trim() !== "") {
-      socket.emit(`${userId}-${professionalId}-chat`, { name, message, sent: true});
+      socket.emit(`${userId}-${professionalId}-chat`, {
+        name,
+        message,
+        sent: true,
+      });
 
       formRef.setFieldValue("message", "");
     }
   };
-
 
   const seen = {};
   const uniqueArray = messages.filter((item) => {
     const key = JSON.stringify(item);
     return seen.hasOwnProperty(key) ? false : (seen[key] = true);
   });
- 
 
   const handleEndButtonClicked = () => {
     if (!exitClicked) {
@@ -141,8 +144,8 @@ const Chat = () => {
 
       clearInterval(timerInterval);
 
-       // Display the chat ended alert message
-       if (!chatEndedMessageDisplayed) {
+      // Display the chat ended alert message
+      if (!chatEndedMessageDisplayed) {
         message.info("Chat has ended.");
         setChatEndedMessageDisplayed(true);
       }
@@ -158,82 +161,99 @@ const Chat = () => {
   //     navigate('/');
   //   }
   // };
-  
 
   return (
+    <>
+      <Navbar />
+      <div className="chatPage">
+        <div className="chatHead">
+          <div className="profileContent">
+            <img src={male_avatar} alt="no image" />
+            <div className="profileInfo">
+              <h6>Subrahmanyam</h6>
+              {/* <span>Balance: (04:35 mins)</span> */}
+              <span>
+                Balance: (
+                {Math.floor(timer / 60)
+                  .toString()
+                  .padStart(2, "0")}
+                :{(timer % 60).toString().padStart(2, "0")} mins)
+              </span>
 
-  <>
-<Navbar/>
- <div className='chatPage'>
-   <div className="chatHead">
-       <div className="profileContent">
-           <img src={male_avatar} alt="no image" />
-           <div className="profileInfo">
-               <h6>Subrahmanyam</h6>
-               {/* <span>Balance: (04:35 mins)</span> */}
-               <span>Balance: ({Math.floor(timer / 60).toString().padStart(2, '0')}:{(timer % 60).toString().padStart(2, '0')} mins)</span>
-
-               <p>{chatEnded ? "Chat has ended" : "Chat in progress"}</p>
-           </div>
-       </div>
-       <div className="endBtn">
+              <p>{chatEnded ? "Chat has ended" : "Chat in progress"}</p>
+            </div>
+          </div>
+          <div className="endBtn">
             {exitClicked ? (
-              <Button className='btn' >
-                EXIT
-              </Button>
+              <Button className="btn">EXIT</Button>
             ) : (
-              <Button className='btn' onClick={handleEndButtonClicked}>
+              <Button className="btn" onClick={handleEndButtonClicked}>
                 END
               </Button>
             )}
-           {/* <Button className='btn' onClick={() => handleEndButtonClicked()}>END</Button> */}
-       </div>
-   </div>
-   <div className="rechargeInfo">
-       <div className="tipInfo">
-           <a href='#'>Balance &lt; 5min Quick Recharge?</a>
-           <p><i className="bi bi-lightbulb-fill"></i> Tip: 90% users recharge for 10min or more</p>
-       </div>
-       <div className="planCost">
-           <Button className='btn1'><i class="bi bi-currency-rupee"></i> 200</Button>
-           <Button className='btn1'><i class="bi bi-currency-rupee"></i> 500</Button>
-           <Button className='btn1'><i class="bi bi-currency-rupee"></i> 1000</Button>
-       </div>
-   </div>
-     <div ref={chatContainerRef} className="message-container">
-       {/* {console.log("JSX messgae", uniqueArray)} */}
-       <List>
-         {uniqueArray.map((msg, index) => (
-            <div key={msg.id || index} className={`message ${msg.sent ? 'sent' : 'received'}`}>
-
-           {/* <div key={index} className={`message ${msg.sent ? 'sent' : 'received'}`} > */}
-             <strong >{msg.name}</strong>: {msg.message}
-           </div>
-         ))}
-       </List>
-     </div>
-   <div className='typeField'>
-       <Form form={formRef}>
-         <Form.Item name="message">
-           <Input.Search
-            enterButton={<SendOutlined onClick={() => {handleSendMessage()}} style={{display: "flex", alignItems: "center", justifyContent: "center"}}/>}
-            placeholder={chatEnded ? "Chat has ended" : "Type a message..."}
-            disabled={chatEnded}
-           />
-         </Form.Item>
-       </Form>
-
-   </div>
- </div>
-<Footer/>
-</> 
-
-
-   
+            {/* <Button className='btn' onClick={() => handleEndButtonClicked()}>END</Button> */}
+          </div>
+        </div>
+        <div className="rechargeInfo">
+          <div className="tipInfo">
+            <a href="#">Balance &lt; 5min Quick Recharge?</a>
+            <p>
+              <i className="bi bi-lightbulb-fill"></i> Tip: 90% users recharge
+              for 10min or more
+            </p>
+          </div>
+          <div className="planCost">
+            <Button className="btn1">
+              <i class="bi bi-currency-rupee"></i> 200
+            </Button>
+            <Button className="btn1">
+              <i class="bi bi-currency-rupee"></i> 500
+            </Button>
+            <Button className="btn1">
+              <i class="bi bi-currency-rupee"></i> 1000
+            </Button>
+          </div>
+        </div>
+        <div ref={chatContainerRef} className="message-container">
+          {/* {console.log("JSX messgae", uniqueArray)} */}
+          <List>
+            {uniqueArray.map((msg, index) => (
+              <div
+                key={msg.id || index}
+                className={`message ${msg.sent ? "sent" : "received"}`}
+              >
+                {/* <div key={index} className={`message ${msg.sent ? 'sent' : 'received'}`} > */}
+                <strong>{msg.name}</strong>: {msg.message}
+              </div>
+            ))}
+          </List>
+        </div>
+        <div className="typeField">
+          <Form form={formRef}>
+            <Form.Item name="message">
+              <Input.Search
+                enterButton={
+                  <SendOutlined
+                    onClick={() => {
+                      handleSendMessage();
+                    }}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  />
+                }
+                placeholder={chatEnded ? "Chat has ended" : "Type a message..."}
+                disabled={chatEnded}
+              />
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
 export default Chat;
-
-
-
